13935339c4173ff7b7e811837a3f6dcca8535d35
RohKaz <rohula-kazimi@hotmail.com>
2023-02-26 18:11:29 +0100

    Aufgabe: Operatoren
        1. Lege zwei Variablen mit int Werten an
        2. Addiere die Werte und gib sie auf der Konsole aus
        3. Führe den String “Summe: “ mit der Ergebnisvariable zusammen und gib ihn aus

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Whats up?");
+
+        int zahl1 = 5;
+        int zahl2 = 10;
+        int ergebnis = zahl1 + zahl2;
+        System.out.println("Summe: " + ergebnis);
     }
 }
